dc7c8b3ebe18b15332ccaa7a9e598794b89d7860
Anzhelika Mihalchenia <anzhelichka1990@gmail.com>
2024-08-04 16:04:36 +0300

    Updated affirmation view and model

---
+++ MyAffirmationsTest/MyAffirmationsTest/MainScreen/Views/AffirmationsView.swift
@@ -0,0 +1,31 @@
+//
+//  CarouselView.swift
+//  MyAffirmationsTest
+//
+//  Created by Анжелика on 2.08.24.
+//
+
+import SwiftUI
+
+struct AffirmationsView: View {
+    let affirmations: [String]
+    
+    var body: some View {
+        VStack {
+            TabView {
+                ForEach(affirmations, id: \.self) { phrase in
+                    Text(phrase)
+                        .font(.largeTitle)
+                        .foregroundColor(.secondary)
+                        .multilineTextAlignment(.center)
+                        .frame(maxWidth: .infinity, maxHeight: .infinity)
+                        .edgesIgnoringSafeArea(.all)
+                        .rotationEffect(.degrees(270))
+                        .padding(.horizontal)
+                }
+            }
+            .rotationEffect(.degrees(90))
+            .tabViewStyle(PageTabViewStyle())
+        }
+    }
+}
