64e8bedc7ef894cfc5cb8f8bd0b055b17bd4d342
Anzhelika Mihalchenia <anzhelichka1990@gmail.com>
2024-08-05 14:27:15 +0300

    Added strings to Localization

--- MyAffirmationsTest/MyAffirmationsTest/Services/Localization/Localization.swift
+++ MyAffirmationsTest/MyAffirmationsTest/Services/Localization/Localization.swift
@@ -12,6 +12,7 @@ enum Localization: String {
     case blue = "blue"
     case category = "category"
     case color = "color"
+    case done = "done"
     case error = "error"
     case female = "female"
     case friendship = "friendship"
@@ -25,7 +26,10 @@ enum Localization: String {
     case selectCategory = "select_category"
     case selectColor = "select_color"
     case selectGender = "select_gender"
+    case selectLanguage = "select_language"
+    case settings = "settings"
     case startButton = "start_button"
+    case stub_label = "stub_label"
     
     var localized: String {
         return NSLocalizedString(self.rawValue, comment: "\(self) could not localized")
